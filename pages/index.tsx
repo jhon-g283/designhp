828bd7d149d5f8474cd267ca983e73e3067664c1
中井孝嘉 <nakaitakashiyoshimi@nakaitakashiyoshiminoMacBook-Pro-2.local>
2025-09-23 19:08:19 +0900

    nodeバージョン変更

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,6 +25,7 @@ const Home: NextPage = () => {
         <a href="./sweep/cart/orderComplete">test7</a>
         <a href="./sweep/news/newsList">test8</a>
         <a href="./sweep/news/newsDetail?id=1">test9</a>
+        {/*  */}
 
         <p className={styles.description}>
           Get started by editing{' '}
